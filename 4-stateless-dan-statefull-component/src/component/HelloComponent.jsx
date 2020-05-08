4e9a46a897b036e8fdc784fcb6154905167bf223
dziljalal1 <dziljalal1@gmail.com>
2020-05-08 14:04:37 +0700

    Statefull dan Stateless
    Component dapat dibuat menggunakan dua cara dengan tipenya masing-masing.
    Biasanya dalam develop dibuatkan foldernya masing-masing.
    
    1. Functional Component / Stateless Component
        Folder : component
        Biasanya bersifat const.
    
    2. Statefull Component
        Folder : container
        Biasanya bersifat class yang di extend dari React.Component
    
    Masing-masing component bisa mempunyai css -nya masing-masing.
    Hal yang perlu diperhatikan adalah tag class pada element HTML dirubah menjadi className.
    
    Contohnya:
            <p class="some-class">Ini adalah text</p> , menjadi
            <p className="some-class">Ini adalah text</p>

---
+++ 4-stateless-dan-statefull-component/src/component/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './HelloComponent.css'
+
+const HelloComponent = () =>{
+    return <p className="text-hello">Hello Functional Component a.k.a Stateless</p>
+}
+
+export default HelloComponent;
---
+++ 4-stateless-dan-statefull-component/src/container/StateFullComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './StateFullComponent.css'
+
+class StateFullComponent extends React.Component {
+    render(){
+        return <p className="text-statefull">Ini adalah StateFullComponent</p>
+    }
+}
+
+export default StateFullComponent;
